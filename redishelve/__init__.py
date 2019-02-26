d35b8b0c3a4227398912911e30fef4632c327a98
Niels van Huijstee <niels.vanhuijstee@cyso.com>
2019-02-26 17:11:54 +0100

    Basic implementation of MutableMapping.
    Implement the methods to make the shelf a MutableMapping object.

--- redishelve/__init__.py
+++ redishelve/__init__.py
@@ -1,11 +1,58 @@
-class RedisShelve:
-    def __init__(self, redis):
+import collections
+import pickle
+
+
+class RedisShelf(collections.abc.MutableMapping):
+    def __init__(self, redis, filename):
+        self._prefix = filename
         self._redis = redis
+        self._keys = []
+
+    def _create_shelf_key(self, key):
+        return "{prefix}|{key}".format(prefix=self._prefix, key=key)
+
+    def _create_key(self, shelf_key):
+        left = '{prefix}|'.format(prefix=self._prefix)
+        key = shelf_key[len(left):]
+        return key
+
+    def _update_keys(self, shelf_key):
+        if shelf_key not in self._keys:
+            self._keys.append(shelf_key)
 
     def __setitem__(self, key, value):
-        print(f'SET KEY {key} VAL {value}')
-        self._redis.set(key, value)
+        shelf_key = self._create_shelf_key(key)
+        self._update_keys(shelf_key)
+        pickled = pickle.dumps(value)
+        self._redis.set(shelf_key, pickled)
 
     def __getitem__(self, key):
-        print(f'GET KEY {key}')
-        return self._redis.get(key)
+        shelf_key = self._create_shelf_key(key)
+        if shelf_key not in self._keys:
+            return None
+
+        pickled = self._redis.get(shelf_key)
+        return pickle.loads(pickled)
+
+    def __delitem__(self, key):
+        shelf_key = self._create_shelf_key(key)
+        self._redis.delete(shelf_key)
+        self._keys.remove(shelf_key)
+
+    def __iter__(self):
+        for shelf_key in self._keys:
+            key = self._create_key(shelf_key)
+            yield key
+
+    def __len__(self):
+        return len(self._keys)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+
+def open(filename, redis):
+    return RedisShelf(redis, filename)
--- tests/test_redishelve.py
+++ tests/test_redishelve.py
@@ -1,18 +1,59 @@
 import fakeredis
 import pytest
-
-from redishelve import RedisShelve
+from redishelve import RedisShelf
 
 
 @pytest.fixture
 def redis():
     fake_server = fakeredis.FakeServer()
     fake_redis = fakeredis.FakeStrictRedis(
-        server=fake_server, decode_responses=True)
+        server=fake_server)
     return fake_redis
 
 
-def test_shelve_value(redis):
-    shelve = RedisShelve(redis)
-    shelve['test'] = 'TEST'
-    assert 'TEST' == shelve['test']
+@pytest.fixture
+def shelf(redis):
+    return RedisShelf(redis, filename='test')
+
+
+def test_shelf_value(shelf):
+    shelf['test'] = 'TEST'
+    assert 'TEST' == shelf['test']
+
+
+def test_shelf_get_method_returns_like_dict(shelf):
+    shelf['test'] = 'TEST'
+    assert 'TEST' == shelf.get('test')
+
+
+def test_shelf_get_returns_none_as_default(shelf):
+    assert None is shelf.get('nothing')
+
+
+def test_shelf_len(shelf):
+    shelf['one'] = 'ONE'
+    shelf['two'] = 'TWO'
+
+    assert 2 == len(shelf)
+
+
+def test_shelf_iteration(shelf):
+    shelf['one'] = 'ONE'
+    shelf['two'] = 'TWO'
+
+    assert ['one', 'two'] == list(shelf.keys())
+    assert ['ONE', 'TWO'] == list(shelf.values())
+    actual = {}
+    for k, v in shelf.items():
+        actual[k] = v
+    assert {'one': 'ONE', 'two': 'TWO'} == actual
+
+
+def test_shelf_delete(shelf):
+    shelf['one'] = 'ONE'
+    shelf['two'] = 'TWO'
+    shelf['three'] = 'THREE'
+
+    del (shelf['two'])
+
+    assert None is shelf.get('two')
