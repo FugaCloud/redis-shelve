5c6182330f97727c50f409bd792af0b34c4e9710
Niels van Huijstee <niels.vanhuijstee@cyso.com>
2019-04-04 17:11:40 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -18,7 +18,12 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'redis',
     ],
